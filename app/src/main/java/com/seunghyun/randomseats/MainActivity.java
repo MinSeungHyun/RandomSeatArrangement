ce9d5873032739171d41d401f243b2398f7d3122
MinSeungHyun <asdf014563@naver.com>
2019-04-11 12:38:20 +0900

    최소화, 최대화 기능 추가

--- app/src/main/java/com/seunghyun/randomseats/MainActivity.java
+++ app/src/main/java/com/seunghyun/randomseats/MainActivity.java
@@ -2,9 +2,11 @@ package com.seunghyun.randomseats;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.SparseIntArray;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.GridView;
@@ -31,18 +33,20 @@ public class MainActivity extends AppCompatActivity {
     GridView seatsGrid;
     IndicatorSeekBar rowSeekBar, columnSeekBar;
     TextView settingLayoutTopTV, okButtonText, cautionTV;
-    RelativeLayout okButton;
-    LinearLayout settingLayout;
+    RelativeLayout okButton, minimizeButton;
+    LinearLayout settingLayout, innerSettingLayout;
     IndicatorStayLayout indicatorLayout;
+    FloatingActionButton floatingButton;
 
     //seatList : grid 를 생성하기 위한 리스트 / shownSeatsList : 숫자가 이미 보여진 자리 리스트 / exceptedList : 사용하지 않는 자리 리스트
     //numberList : seatList 에서 사용하지 않는 자리를 제외하고, 랜덤 숫자가 들어갈 리스트(사용하지 않는 자리에는 -1이 들어감) / arrayedNumberList : 정렬된 numberList
     ArrayList<Integer> seatList, shownSeatsList, exceptedList, numberList, arrayedNumberList;
     //fixedSeatsMap : 고정된 자리<Integer position, Integer number>
     SparseIntArray fixedSeatsMap;
-    TranslateAnimation outAnimation, inAnimation;
+    AlphaAnimation fadeOut, fadeIn;
+    TranslateAnimation minimize, expand;
     int stage = 0; //단계, 애니메이션 전환에 사용
-
+    boolean isMinimized;
     private InterstitialAd interstitialAd;
 
     @Override
@@ -50,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //광고 초기화
+        //Init ad
         AdView adView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
@@ -58,10 +62,12 @@ public class MainActivity extends AppCompatActivity {
         interstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
         interstitialAd.loadAd(new AdRequest.Builder().build());
 
+        //Init
         EventBus.getDefault().register(this);
         initialize();
         makeGrid(SeatsGirdAdapter.INITIALIZE);
 
+        //Set listener
         final OnSeekChangeListener seekChangeListener = new OnSeekChangeListener() {
             @Override
             public void onSeeking(SeekParams seekParams) {
@@ -80,13 +86,50 @@ public class MainActivity extends AppCompatActivity {
         };
         rowSeekBar.setOnSeekChangeListener(seekChangeListener);
         columnSeekBar.setOnSeekChangeListener(seekChangeListener);
-
         okButton.setOnClickListener(v -> {
-            settingLayout.startAnimation(outAnimation);
+            innerSettingLayout.startAnimation(fadeOut);
             stage = 1;
         });
+        seatsGrid.setOnItemClickListener((parent, view, position, id) -> {
+            boolean isNotUseSeatChecked = exceptedList.contains(position);
+            DetailSettingDialog dialog = new DetailSettingDialog(MainActivity.this, isNotUseSeatChecked, position, arrayedNumberList, fixedSeatsMap);
+            dialog.show();
+        });
 
-        outAnimation.setAnimationListener(new Animation.AnimationListener() {
+        minimizeButton.setOnClickListener(v -> {
+            settingLayout.startAnimation(minimize);
+            isMinimized = true;
+        });
+        floatingButton.setOnClickListener(v -> {
+            innerSettingLayout.setVisibility(View.VISIBLE);
+            minimizeButton.setVisibility(View.VISIBLE);
+            settingLayout.startAnimation(expand);
+            isMinimized = false;
+        });
+        Animation.AnimationListener minimizeAnimListener = new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (isMinimized) {
+                    innerSettingLayout.setVisibility(View.GONE);
+                    minimizeButton.setVisibility(View.GONE);
+                    floatingButton.show();
+                } else floatingButton.hide();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        };
+        minimize.setAnimationListener(minimizeAnimListener);
+        expand.setAnimationListener(minimizeAnimListener);
+
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
             }
@@ -103,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
                     okButton.setOnClickListener(v -> {
                         makeGrid(SeatsGirdAdapter.SHOW_ALL);
                         seatsGrid.setOnItemClickListener(null);
-                        settingLayout.startAnimation(outAnimation);
+                        innerSettingLayout.startAnimation(fadeOut);
                         stage = 2;
                     });
                     seatsGrid.setOnItemClickListener((parent, view, position, id) -> {
@@ -122,17 +165,17 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                     customShuffle(numberList);
-                    settingLayout.startAnimation(inAnimation);
+                    innerSettingLayout.startAnimation(fadeIn);
                 } else if (stage == 2) {
                     //전체 보기 버튼을 눌렀을 때
                     settingLayoutTopTV.setVisibility(View.GONE);
                     cautionTV.setVisibility(View.GONE);
                     okButtonText.setText(getString(R.string.reset));
                     okButton.setOnClickListener(v -> {
-                        settingLayout.startAnimation(outAnimation);
+                        innerSettingLayout.startAnimation(fadeOut);
                         stage = 0;
                     });
-                    settingLayout.startAnimation(inAnimation);
+                    innerSettingLayout.startAnimation(fadeIn);
                 } else if (stage == 0) {
                     //초기화 버튼 눌렀을 때
                     if (interstitialAd.isLoaded()) //전면광고
@@ -148,7 +191,7 @@ public class MainActivity extends AppCompatActivity {
                     indicatorLayout.setVisibility(View.VISIBLE);
                     okButtonText.setText(getString(R.string.ok));
                     okButton.setOnClickListener(v -> {
-                        settingLayout.startAnimation(outAnimation);
+                        innerSettingLayout.startAnimation(fadeOut);
                         stage = 1;
                     });
                     seatsGrid.setOnItemClickListener((parent, view, position, id) -> {
@@ -157,19 +200,13 @@ public class MainActivity extends AppCompatActivity {
                         dialog.show();
                     });
                     makeGrid(SeatsGirdAdapter.INITIALIZE);
-                    settingLayout.startAnimation(inAnimation);
+                    innerSettingLayout.startAnimation(fadeIn);
                 }
             }
 
             @Override
             public void onAnimationRepeat(Animation animation) {
             }
-        });
-
-        seatsGrid.setOnItemClickListener((parent, view, position, id) -> {
-            boolean isNotUseSeatChecked = exceptedList.contains(position);
-            DetailSettingDialog dialog = new DetailSettingDialog(MainActivity.this, isNotUseSeatChecked, position, arrayedNumberList, fixedSeatsMap);
-            dialog.show();
         });
     }
 
@@ -256,21 +293,29 @@ public class MainActivity extends AppCompatActivity {
         rowSeekBar.setIndicatorTextFormat(getString(R.string.row) + "${PROGRESS}");
         columnSeekBar.setIndicatorTextFormat(getString(R.string.column) + "${PROGRESS}");
         cautionTV = findViewById(R.id.caution_tv);
+        minimizeButton = findViewById(R.id.minimize_button);
+        floatingButton = findViewById(R.id.floating_button);
+        innerSettingLayout = findViewById(R.id.inner_setting_layout);
+        floatingButton.hide();
 
-        outAnimation = new TranslateAnimation(
+        fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setDuration(500);
+        fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setDuration(500);
+        minimize = new TranslateAnimation(
                 Animation.RELATIVE_TO_SELF, 0,
                 Animation.RELATIVE_TO_SELF, 0,
                 Animation.RELATIVE_TO_SELF, 0,
                 Animation.RELATIVE_TO_SELF, 1.1f);
-        outAnimation.setDuration(500);
-        outAnimation.setFillAfter(true);
-        inAnimation = new TranslateAnimation(
+        minimize.setDuration(500);
+        minimize.setFillAfter(true);
+        expand = new TranslateAnimation(
                 Animation.RELATIVE_TO_SELF, 0,
                 Animation.RELATIVE_TO_SELF, 0,
                 Animation.RELATIVE_TO_SELF, 1.1f,
                 Animation.RELATIVE_TO_SELF, 0);
-        inAnimation.setDuration(500);
-        inAnimation.setFillAfter(true);
+        expand.setDuration(500);
+        expand.setFillAfter(true);
 
         seatList = new ArrayList<>();
         shownSeatsList = new ArrayList<>();
@@ -278,6 +323,8 @@ public class MainActivity extends AppCompatActivity {
         numberList = new ArrayList<>();
         arrayedNumberList = new ArrayList<>();
         fixedSeatsMap = new SparseIntArray();
+
+        isMinimized = false;
     }
 
     private void customShuffle(ArrayList<Integer> list) {
