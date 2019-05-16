eb3f310ee2118e392670731854e70ab9fbea67ad
MinSeungHyun <asdf014563@naver.com>
2019-05-16 13:10:14 +0900

    RecyclerView 구현

--- app/src/main/java/com/seunghyun/randomseats/HistoryFragment.java
+++ app/src/main/java/com/seunghyun/randomseats/HistoryFragment.java
@@ -3,11 +3,17 @@ package com.seunghyun.randomseats;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class HistoryFragment extends Fragment {
+    RecyclerView recyclerView;
+
     public HistoryFragment() {
     }
 
@@ -18,6 +24,18 @@ public class HistoryFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+
+        ArrayList<CardItem> items = new ArrayList<>();
+        items.add(new CardItem(null, "Title1", "Description1", "2019.05.16"));
+        items.add(new CardItem(null, "Title2", "Description2", "2019.05.17"));
+
+        recyclerView.setAdapter(new RecyclerAdapter(items));
+
+        return view;
     }
 }
