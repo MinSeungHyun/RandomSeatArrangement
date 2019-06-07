85c2a2e522f7ee24d51002dc866615f74c443d8f
MinSeungHyun <asdf014563@naver.com>
2019-06-07 20:37:44 +0900

    전체화면모드, 터치시 뒤로가가버튼 사라지는 기능 추가

--- app/src/main/java/com/seunghyun/randomseatarrangement/activities/FullScreenActivity.kt
+++ app/src/main/java/com/seunghyun/randomseatarrangement/activities/FullScreenActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.seunghyun.randomseatarrangement.R
 import com.seunghyun.randomseatarrangement.fragments.HomeFragment
@@ -13,16 +14,29 @@ class FullScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_full_screen)
         backButton.setOnClickListener { finish() }
+        photoView.setOnClickListener {
+            if (virtualActionBar.visibility == View.VISIBLE) {
+                virtualActionBar.visibility = View.GONE
+                backButton.visibility = View.GONE
+            } else {
+                virtualActionBar.visibility = View.VISIBLE
+                backButton.visibility = View.VISIBLE
+            }
+        }
 
         val bitmaps = HomeFragment.requestGridBitmap.getBitmaps()
-        val bitmap = Bitmap.createBitmap(bitmaps[2].width + bitmaps[0].width, bitmaps[1].height + bitmaps[0].height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        canvas.drawBitmap(bitmaps[1], bitmaps[2].width.toFloat(), 0f, null)
-        canvas.drawBitmap(bitmaps[2], 0f, bitmaps[1].height.toFloat(), null)
-        canvas.drawBitmap(bitmaps[0], bitmaps[2].width.toFloat(), bitmaps[1].height.toFloat(), null)
-        photoView.setImageBitmap(bitmap)
+        if (bitmaps.size == 1) photoView.setImageBitmap(bitmaps[0])
+        else {
+            val bitmap = Bitmap.createBitmap(bitmaps[2].width + bitmaps[0].width, bitmaps[1].height + bitmaps[0].height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            canvas.drawBitmap(bitmaps[1], bitmaps[2].width.toFloat(), 0f, null)
+            canvas.drawBitmap(bitmaps[2], 0f, bitmaps[1].height.toFloat(), null)
+            canvas.drawBitmap(bitmaps[0], bitmaps[2].width.toFloat(), bitmaps[1].height.toFloat(), null)
+            photoView.setImageBitmap(bitmap)
+        }
     }
 
     companion object {
