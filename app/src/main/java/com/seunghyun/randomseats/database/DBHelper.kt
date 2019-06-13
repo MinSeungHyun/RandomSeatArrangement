2bcbe7e75a8cf98413f850e7d0121ad190a5f68a
MinSeungHyun <asdf014563@naver.com>
2019-06-13 22:33:11 +0900

    계약 class 구현

--- app/src/main/java/com/seunghyun/randomseats/database/DBHelper.kt
+++ app/src/main/java/com/seunghyun/randomseats/database/DBHelper.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DBHelper(val context: Context, val name: String, val factory: SQLiteDatabase.CursorFactory, val version: Int) : SQLiteOpenHelper(context, name, factory, version) {
-    override fun onCreate(db: SQLiteDatabase?) {
+const val DB_VERSION = 1
+const val DB_FILE = "history.db"
 
+class DBHelper(context: Context) : SQLiteOpenHelper(context, DB_FILE, null, DB_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
