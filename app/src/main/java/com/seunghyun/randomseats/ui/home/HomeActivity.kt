6575d5af2cb1b64641a02e8c9c7291a1c835614f
SeungHyunMin <asdf014563@naver.com>
2020-01-11 00:18:13 +0900

    Rename Activity and change package

--- app/src/main/java/com/seunghyun/randomseats/ui/home/HomeActivity.kt
+++ app/src/main/java/com/seunghyun/randomseats/ui/home/HomeActivity.kt
@@ -1,12 +1,13 @@
-package com.seunghyun.randomseats
+package com.seunghyun.randomseats.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.seunghyun.randomseats.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
