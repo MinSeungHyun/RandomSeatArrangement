431ea509f9a33f145ec012aeadc112d551622d28
SeungHyunMin <asdf014563@naver.com>
2020-01-11 12:53:39 +0900

    Create settings icon in action bar

--- app/src/main/java/com/seunghyun/randomseats/ui/home/HomeActivity.kt
+++ app/src/main/java/com/seunghyun/randomseats/ui/home/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.seunghyun.randomseats.ui.home
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.seunghyun.randomseats.R
 
@@ -9,5 +11,16 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setTitle(R.string.home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.settings -> true
+        else -> super.onOptionsItemSelected(item)
     }
 }
