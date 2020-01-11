14dbe2b728df424380e2a25b4148bbef1f2d7b4a
SeungHyunMin <asdf014563@naver.com>
2020-01-11 13:53:21 +0900

    Create ArrangementActivity

---
+++ app/src/main/java/com/seunghyun/randomseats/ui/arrangement/ArrangementActivity.kt
@@ -0,0 +1,12 @@
+package com.seunghyun.randomseats.ui.arrangement
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.seunghyun.randomseats.R
+
+class ArrangementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arrangment)
+    }
+}
