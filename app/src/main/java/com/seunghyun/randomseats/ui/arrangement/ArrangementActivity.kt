0dd3ceeb18325999923f1cdaf1a678ac37edea40
SeungHyunMin <asdf014563@naver.com>
2020-01-26 16:16:33 +0900

    Apply data binding to ArrangementActivity

--- app/src/main/java/com/seunghyun/randomseats/ui/arrangement/ArrangementActivity.kt
+++ app/src/main/java/com/seunghyun/randomseats/ui/arrangement/ArrangementActivity.kt
@@ -1,13 +1,18 @@
 package com.seunghyun.randomseats.ui.arrangement
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.seunghyun.randomseats.R
+import com.seunghyun.randomseats.databinding.ActivityArrangmentBinding
+import com.seunghyun.randomseats.ui.BindingActivity
 
-class ArrangementActivity : AppCompatActivity() {
+class ArrangementActivity : BindingActivity<ActivityArrangmentBinding>(R.layout.activity_arrangment) {
+    private val viewModel by lazy { ArrangementViewModel() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTitle(R.string.arrangement)
-        setContentView(R.layout.activity_arrangment)
+        binding.apply {
+            vm = viewModel
+        }
     }
 }
---
+++ app/src/main/java/com/seunghyun/randomseats/ui/arrangement/ArrangementViewModel.kt
@@ -0,0 +1,3 @@
+package com.seunghyun.randomseats.ui.arrangement
+
+class ArrangementViewModel
